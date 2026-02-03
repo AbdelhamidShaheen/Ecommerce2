4b4628d189fadae48351e00ec98232322d52f71c
AbdelhamidShaheen <abdelhamid_shaheen@yahoo.com>
2026-02-03 06:40:21 +0200

    finish setup redis

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    Cache::put('key', "wellcome redis");
-    dd(Cache::get('key', 'default'));
     return view('welcome');
 });
